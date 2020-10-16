24f859bdd91fd587e8c836593ce04bd0c9903488
Alejandro Lazo Hernandez <alazo@Alejandros-MacBook-Pro.local>
2020-10-16 12:38:10 -0400

    add main routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
 import './App.css';
 import CreateTournament from './Components/CreateTournament';
+import TournamentDashboard from './Components/TournamentDashboard';
 import AppContext from './Context/appContext';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   const [tournaments, setTournaments] = useState([]);
@@ -13,7 +15,11 @@ function App() {
   return (
     <AppContext.Provider value={{tournaments, onCreateTournament: createTournamentHandle}}>
       <div className="App">
-        <CreateTournament></CreateTournament>
+        <Switch>
+          <Route path="/createTournament" component={CreateTournament}></Route>
+          <Route path="/TournamentDashboard" component={TournamentDashboard}></Route>
+          <Route path="/" component={CreateTournament}></Route>
+        </Switch>
       </div>
     </AppContext.Provider>
   );
---
+++ src/Components/TournamentDashboard/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TournamentDashboard = () => {
+  return ( 
+    <div>
+      Matches
+    </div>
+   );
+}
+ 
+export default TournamentDashboard;
