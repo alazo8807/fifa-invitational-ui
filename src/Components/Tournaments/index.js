33adafa6d1cde3867b828ce46d648e118cf5dfb0
Alejandro Lazo Hernandez <alazo8807@gmail.com>
2020-10-29 17:01:40 -0400

    add initial tournaments route and page

---
+++ src/Components/Tournaments/index.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TournamentCard from './TournamentCard';
+import { getTournaments } from '../../Services/tournamentService';
+import { Grid } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    margin: theme.spacing(1)
+  },
+}));
+
+const TournamentsList = () => {
+  const classes = useStyles();
+  const [tournaments, setTournaments] = useState([]);
+
+  useEffect(()=>{
+    const getTournamentsFromDb = async () => {
+      const result = await getTournaments();
+      const tournaments = result.data;
+      
+      setTournaments(tournaments);
+      console.log(tournaments);
+    }
+  
+    getTournamentsFromDb();
+  },[])
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={5} justify='center'>
+        {tournaments.map(tournament => (
+          <Grid item>
+            <TournamentCard></TournamentCard>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+   );
+}
+ 
+export default TournamentsList;
